13b12d68fcd8ccc0e97e10e55c0cee498f373c34
Vadim Sevruk <v.sevrukas@gmail.com>
2018-03-09 13:50:46 +0200

    initial

---
+++ script.js
@@ -0,0 +1,44 @@
+// 1
+alert('Hello');
+
+// 2
+var firstName = 'Alex';
+
+var lastName = 'Alex';
+
+document.write(lastName);
+
+// 3
+var firstName = 'Jon';
+
+var lastName = 'Snow';
+
+var age = 22;
+
+var education =  "Night's Watch";
+
+var hobby = 'Winter sports';
+
+// 4
+
+// 1) 2
+
+// 2) 1
+
+// 3) 5
+
+// 4) 4
+
+// 5) 3
+
+// 6) 3
+
+// 5
+
+var x1 = +prompt('num1');
+
+var x2 = +prompt('num2');
+
+var sum = x1 +x2;
+
+alert(sum);
